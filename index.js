b9634ac2efd4026d2c2d025c83054e917689e485
Joel Jacob <joeljacobdev@gmail.com>
2020-05-29 10:08:12 +0530

    Added draft PR filter. Some changes to template and readme.

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const core = require("@actions/core");
 
-const handlePullRequest = require("./lib/handle_pull_request");
 const handleSchedule = require("./lib/handle_schedule");
 
 main();
 
 async function main() {
-  if (process.env.GITHUB_EVENT_NAME === "pull_request") {
-    return handlePullRequest();
-  }
-
   handleSchedule();
 }
 
